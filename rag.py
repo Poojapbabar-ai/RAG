2bc165dfd80ccc38d5d5d84659b1c3a0753572e7
Poojapbabar-ai <babarpooja2002@gmail.com>
2026-03-15 15:22:45 +0530

    CI

--- rag.py
+++ rag.py
@@ -95,15 +95,10 @@ Context:
 Question: {query}
 
 Answer:"""
-    
-    # response = client.chat.completions.create(
-    #     model="llama-3.3-70b-versatile",
-    #     messages=[{"role": "user", "content": prompt}]
-    # )
     response = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
     messages=[{"role": "user", "content": prompt}],
-    temperature=0.2
+    temperature=0.9
 )
     return response.choices[0].message.content
 
